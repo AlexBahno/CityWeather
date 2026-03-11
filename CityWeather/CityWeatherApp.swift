3f8db034a54ba3bef8a8bd09ff5dbb433ec245f2
Alex <Bahno>
2026-03-11 21:47:56 +0100

    Initial commit

---
+++ CityWeather/CityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherApp.swift
+//  CityWeather
+//
+//  Created by Alexandr Bahno on 11.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
